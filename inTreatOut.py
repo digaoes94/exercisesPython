59b3228f39ca884270aa85395176679bab0d334b
Rodrigo Araujo <rasch.dev@gmail.com>
2026-02-11 23:21:07 -0300

    Question 2 done

--- inTreatOut.py
+++ inTreatOut.py
@@ -1,3 +1,21 @@
 # Question 1. Ask the User for a number, read it and then print "The chosen number was:"
-number = float(input("Type a number, whole or broken\n"))
-print(f"The chosen number was: {number}")
+# number = float(input("Type a number, whole or broken\n"))
+# print(f"The chosen number was: {number}")
+
+# Question 2. Ask for four grades and print the student's final grade
+finalGrade = 0.0
+grades = 0
+
+finalGrade += float(input("Grade1: "))
+grades += 1
+
+finalGrade += float(input("Grade2: "))
+grades += 1
+
+finalGrade += float(input("Grade3: "))
+grades += 1
+
+finalGrade += float(input("Grade4: "))
+grades += 1
+
+print(f"Student's final grade = {finalGrade/grades}")
