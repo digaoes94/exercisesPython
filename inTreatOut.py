f7d9dec2319ce5c88ea2e8f90a00bdec4b82b42d
Rodrigo Araujo <rasch.dev@gmail.com>
2026-02-11 23:10:50 -0300

    Question 1 done

--- inTreatOut.py
+++ inTreatOut.py
@@ -1,1 +1,3 @@
 # Question 1. Ask the User for a number, read it and then print "The chosen number was:"
+number = float(input("Type a number, whole or broken\n"))
+print(f"The chosen number was: {number}")
