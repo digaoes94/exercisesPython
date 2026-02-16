0a131175b5b7c825af9eec88703a334c30562591
Rodrigo Araujo <rasch.dev@gmail.com>
2026-02-16 17:49:33 -0300

    Question 3 done

--- dataStructures.py
+++ dataStructures.py
@@ -31,32 +31,50 @@
 
 # Question 2. Read 4 integer numbers, add them to a tuple and then print:
 # a) how many times the n9 appears in the tuple; b) the index from the first n3; c) which numbers are odd
-aList = []; odds = []; a = 1
+# aList = []; odds = []; a = 1
 
-while a < 5:
-    print(f"Inform number {a}: ")
-    x = int(input())
+# while a < 5:
+#     print(f"Inform number {a}: ")
+#     x = int(input())
 
-    if (x % 2) == 0:
-        odds.append(x)
+#     if (x % 2) == 0:
+#         odds.append(x)
     
-    aList.append(x)
-    a += 1
+#     aList.append(x)
+#     a += 1
 
-aTuple = tuple(aList)
+# aTuple = tuple(aList)
 
-print("\n=====  N° of time the number 9 appears  =====")
-n9 = aTuple.count(9)
-print(f"The number 9 appears {n9} times within the tuple")
+# print("\n=====  N° of time the number 9 appears  =====")
+# n9 = aTuple.count(9)
+# print(f"The number 9 appears {n9} times within the tuple")
 
-print("\n=====  Index from first occurence from the number 3  =====")
-n3 = 0
-try:
-    print(f"First occurence of number 3 happens at index {aTuple.index(3)}")
-except:
-    print("The number 3 doesn't appear within the tuple")
+# print("\n=====  Index from first occurence from the number 3  =====")
+# n3 = 0
+# try:
+#     print(f"First occurence of number 3 happens at index {aTuple.index(3)}")
+# except:
+#     print("The number 3 doesn't appear within the tuple")
 
-print("\n=====  Odd numbers  =====")
-for x in odds:
+# print("\n=====  Odd numbers  =====")
+# for x in odds:
+#     print(x)
+
+# Question 3. Create a program where the User can provide N numbers and insert those in a list. If the number already exists in the list, do not add it.
+# After all N numbers where informed, print them in ascendent manner.
+
+n = int(input("How many numbers will you digit?"))
+list = []
+
+for m in range(n):
+    x = int(input(f"Inform the number {m+1}: "))
+
+    if x not in list:
+        list.append(x)
+    
+    m += 1
+
+list.sort()
+
+for x in list:
     print(x)
-
